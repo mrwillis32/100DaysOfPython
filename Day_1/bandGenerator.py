f22cb697279bffdb158b42486f37391fd969e8e6
Henry J. Willis Jr <83623388+mrwillis32@users.noreply.github.com>
2023-11-11 23:26:26 -0500

    Working with python variables and inputs
    Building a simple band name generator using python variables and inputs to generate band names.

---
+++ Day_1/bandGenerator.py
@@ -0,0 +1,5 @@
+print("Hello, so you want to start a band and need a cool name? Give me a shot!")
+user_city = input("What city did you grown up in?")
+user_pet = input("What's your pet name?")
+band_name = user_city + user_pet
+print("Your band name is... " + user_city + " " + user_pet)
